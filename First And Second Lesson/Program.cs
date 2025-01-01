0c2dd42e5a4ff9ba0b999a1821934bcd91586aef
b m <mukarrammukarramov09@gmail.com>
2025-01-01 11:45:43 +0500

    Добавьте файлы проекта.

---
+++ First And Second Lesson/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int Num = 123;
+        int FirstOfNum = Num / 100;
+        int SecondOfNum = Num / 10 % 10;
+        int EndOfNum = Num % 10;
+        Console.WriteLine($" Num = {Num}\n First Of Num: {FirstOfNum}\n Second Of Num: {SecondOfNum}\n End Of Num: {EndOfNum}");
+    }
+}
